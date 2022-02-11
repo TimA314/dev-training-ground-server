05fa4bfa74806b9f236910b7acb35a25c9048052
TimA314 <ci0d7j9m@mailer.me>
2022-02-11 13:45:49 -0800

    add get method for quiz

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const quiz = require("./Data/quizeQuestions.json");
+
 app.listen(PORT, function () {
   console.log("Listening on PORT " + PORT);
 });
 
 app.get("/ping", (req, res) => {
   res.send("success");
+});
+
+app.get("/quiz", (req, res) => {
+  res.send(quiz);
 });
