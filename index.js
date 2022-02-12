e1e2b9989ecc95f4db2f752aca546cf9f89e46f0
TimA314 <ci0d7j9m@mailer.me>
2022-02-12 09:07:49 -0800

    configure for heroku deployment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = 8080;
+const port = process.env.PORT || 8080;
 
 const quiz = require("./Data/quizQuestions.json");
 const tutorials = require("./Data/tutorials.json");
@@ -7,41 +7,43 @@ const tutorials = require("./Data/tutorials.json");
 const app = express();
 
 // ------ to allow cross origin calls in dev
-const originPort = 3000; // Front End (React) local port number
-app.use(function (req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    `http://localhost:${originPort}`
-  );
+if (process.env.NODE_ENV !== "production") {
+  const originPort = process.env.originPORT || 3000; // Front End (React) local port number
+  app.use(function (req, res, next) {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      `http://localhost:${originPort}`
+    );
 
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
 
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
 
-  res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Credentials", true);
 
-  next();
-});
-// -------
+    next();
+  });
+  // -------
 
-app.get("/ping", (req, res) => {
-  res.send("success");
-});
+  app.get("/ping", (req, res) => {
+    res.send("success");
+  });
 
-app.get("/quiz", (req, res) => {
-  res.send(quiz);
-});
+  app.get("/quiz", (req, res) => {
+    res.send(quiz);
+  });
 
-app.get("/tutorials", (req, res) => {
-  res.send(tutorials);
-});
+  app.get("/tutorials", (req, res) => {
+    res.send(tutorials);
+  });
+}
 
 app.listen(PORT, function () {
-  console.log("Listening on PORT " + PORT);
+  console.log("Listening on PORT " + port);
 });
