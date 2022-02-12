50ff5b7d5c635dd6ac8e7a68bf36567f62d7d403
TimA314 <ci0d7j9m@mailer.me>
2022-02-12 08:21:39 -0800

    add tutorial data and calls

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require("express");
-const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 const quiz = require("./Data/quizeQuestions.json");
+const tutorials = require("./Data/tutorials.json");
 
-app.listen(PORT, function () {
-  console.log("Listening on PORT " + PORT);
+const app = express();
+
+// ------ to allow cross origin calls in dev
+const originPort = 3001; // Front End (React) local port number
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    `http://localhost:${originPort}`
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
 });
+// -------
 
 app.get("/ping", (req, res) => {
   res.send("success");
@@ -14,4 +36,12 @@ app.get("/ping", (req, res) => {
 
 app.get("/quiz", (req, res) => {
   res.send(quiz);
+});
+
+app.get("/tutorials", (req, res) => {
+  res.send(tutorials);
+});
+
+app.listen(PORT, function () {
+  console.log("Listening on PORT " + PORT);
 });
